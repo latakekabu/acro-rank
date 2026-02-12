ad32dc2bd2dfbc0462e14326e1546597023610e3
latakekabu <latakekabu@gmail.com>
2026-02-12 10:21:43 +0300

    feat: advanced 3D sport depth

--- js/main.js
+++ js/main.js
@@ -98,4 +98,33 @@ color
 
 });
 
+/* ===== ADVANCED 3D ===== */
+
+const blocks=document.querySelectorAll(".block");
+
+blocks.forEach(block=>{
+  block.addEventListener("mousemove",(e)=>{
+    const rect=block.getBoundingClientRect();
+    const x=e.clientX-rect.left;
+    const y=e.clientY-rect.top;
+
+    block.style.setProperty("--x",x+"px");
+    block.style.setProperty("--y",y+"px");
+
+    const centerX=rect.width/2;
+    const centerY=rect.height/2;
+
+    const rotateX=((y-centerY)/centerY)*8;
+    const rotateY=((x-centerX)/centerX)*-8;
+
+    block.style.transform=`rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  });
+
+  block.addEventListener("mouseleave",()=>{
+    block.style.transform="rotateX(0deg) rotateY(0deg)";
+  });
 });
+
+  
+});
+
